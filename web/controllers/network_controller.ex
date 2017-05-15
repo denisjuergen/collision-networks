42519586e5f652c7d1615cd3777301f2e10e27a0
Denis Brat <denis.juergen@gmail.com>
2017-05-15 01:33:11 -0300

    Extracted common algorithm between endpoints

--- web/controllers/network_controller.ex
+++ web/controllers/network_controller.ex
@@ -8,19 +8,23 @@ defmodule NetworkCollisions.NetworksController do
   end
 
   def update(conn, %{"nodes" => [node | more]}) do
-    network = Enum.reduce(more, Network.make(node), &Network.include(&2, &1))
+    network = network(node, more)
     updated = NetworkCollisions.Store.get |> NetworkSet.append(network)
     NetworkCollisions.Store.update(updated)
     json conn, NetworkSet.flatten(updated)
   end
 
   def collides(conn, %{"nodes" => [node | more]}) do
-    network = Enum.reduce(more, Network.make(node), &Network.include(&2, &1))
+    network = network(node, more)
     json conn, NetworkCollisions.Store.get |> NetworkSet.contains(network)
   end
 
   def intersects(conn, %{"nodes" => [node | more]}) do
-    network = Enum.reduce(more, Network.make(node), &Network.include(&2, &1))
+    network = network(node, more)
     json conn, NetworkCollisions.Store.get |> NetworkSet.containsAny(network)
+  end
+
+  defp network(node, more) do
+    Enum.reduce(more, Network.make(node), &Network.include(&2, &1))
   end
 end
